f22fed978d65759e30e9c9e0b1d8641eecd4e4dc
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2023-09-21 14:07:35 +0300

    Update CarDetail.tsx

--- components/CarDetail.tsx
+++ components/CarDetail.tsx
@@ -36,9 +36,12 @@ const CarDetail = ({ isOpen, closeModal, car }: CarDetailProps) => {
                                 leaveFrom="opacity-100 scale-100 "
                                 leaveTo="opacity-0 scale-95"
                             >
-                                <Dialog.Panel>
+                                <Dialog.Panel
+                                    className="relative w-full max-w-lg max-h-[90vh] overflow-auto transform rounded-2xl bg-white text-left shadow-xl transition-all flex flex-col gap-5"
+                                >
                                     <button type="button"
                                         onClick={closeModal}
+                                        className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
                                     >
                                         <Image
                                             src="/close.svg"
@@ -48,6 +51,11 @@ const CarDetail = ({ isOpen, closeModal, car }: CarDetailProps) => {
                                             className="object-contain"
                                         />
                                     </button>
+                                    <div className="flex-1 flex flex-col gap-3">
+                                        <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg " >
+
+                                        </div>
+                                    </div>
                                 </Dialog.Panel>
                             </Transition.Child>
                         </div>
@@ -57,5 +65,4 @@ const CarDetail = ({ isOpen, closeModal, car }: CarDetailProps) => {
         </>
     )
 }
-
 export default CarDetail;
