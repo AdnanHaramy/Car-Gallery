ebf7b91ecaff43fbdc0dae915e66e0b065b69cc7
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2023-09-21 13:50:18 +0300

    Update CarCard.tsx

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,12 +4,14 @@ import Image from "next/image";
 import { CustomButton } from "@components";
 import { CarProps } from "@types";
 import { calculateCarRent } from "@utils";
+import CarDetail from "./CarDetail";
 interface CarCardProps {
     car: CarProps;
 }
 const CarCard = ({ car }: CarCardProps) => {
-    const { city_mpg, year, make, model, transmission } = car;
+    const { city_mpg, year, make, model, transmission, drive } = car;
     const carRent = calculateCarRent(city_mpg, year);
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className="car-card group">
             <div className="car-card__content">
@@ -38,8 +40,34 @@ const CarCard = ({ car }: CarCardProps) => {
                             {transmission === 'a' ? 'Automatic' : 'Manual'}
                         </p>
                     </div>
+                    <div className="flex flex-col justify-center items-center gap-2 " >
+                        <Image src="/tire.svg" width={20} height={20} alt="tire" />
+                        <p className="text-[14px]">
+                            {drive.toUpperCase()}
+                        </p>
+                    </div>
+                    <div className="flex flex-col justify-center items-center gap-2 " >
+                        <Image src="/gas.svg" width={20} height={20} alt="gas" />
+                        <p className="text-[14px]">
+                            {city_mpg} MPG
+                        </p>
+                    </div>
+                </div>
+                <div className="car-card__btn-container">
+                    <CustomButton
+                        title="View More"
+                        containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+                        textStyles="text-white text-[14px] leading-[17px] font-bold"
+                        rightIcon="/right-arrow.svg"
+                        handleClick={() => setIsOpen(true)}
+                    />
                 </div>
             </div>
+            <CarDetail
+                isOpen={isOpen}
+                closeModal={() => setIsOpen(false)}
+                car={car}
+            />
         </div>
     )
 }
